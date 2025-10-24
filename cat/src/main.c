1319ffe32027639c68b5519f1338fc706d5cfc66
Hugo Chen <hugo.chen731@gmail.com>
2025-10-25 01:32:54 +0800

    first working cat

--- cat/src/main.c
+++ cat/src/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
   if (argc < 2 ) {
     printf("No arguments, please try again.\n");
     return 1;
   }
-  printf("Hello World!\n");
+  for (int i = 1; i < argc; ++i) {
+    char* fileName = argv[i];
+    FILE *fptr = fopen(fileName, "r");
+    if (fptr == NULL) {
+      printf("Not able to open file: %s\n", fileName);
+      return 1;
+    }
+    char ch;
+    while ((ch = fgetc(fptr)) != EOF) {
+      printf("%c", ch);
+    }
+    fclose(fptr);
+  }
+  printf("\n");
   return 0;
 }
 
